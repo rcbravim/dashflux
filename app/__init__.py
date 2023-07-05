f903da0943313d18e85e1882ac924fcf2a0a8b1d
Raphael Bravim <raphael.bravim@gmail.com>
2023-07-05 00:13:55 -0300

    melhoria de arquitetura, e refatoração de código

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,25 @@
-import click
 from flask import Flask
 from app.config import Config
 from app.auth.routes import bp as auth_bp
+from app.board.routes import bp as board_bp
+from app.auth import models
+from app.board import models
+from app.db.database import init_db, db
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(Config(app))
 
-    from app.auth import models
-    from app.board import models
-    from app.db.database import init_db, db
+    # drop and create db (if init-db command)
+    init_db(app)
 
-    @app.cli.command('init-db')
-    def init_db_command():
-        init_db(app)
-        click.echo('Initialized the database.')
-
-    from app.db.database import db
+    # init app with sqlalchemy instance
     db.init_app(app)
 
+    # register blueprints
     app.register_blueprint(auth_bp)
+    app.register_blueprint(board_bp)
 
     return app
 
--- app/db/database.py
+++ app/db/database.py
@@ -1,9 +1,13 @@
+import click
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 
 def init_db(app):
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
+    @app.cli.command('init-db')
+    def init_db_command():
+        with app.app_context():
+            db.drop_all()
+            db.create_all()
+            click.echo('Initialized the database.')
