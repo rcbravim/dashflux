bf17d6ec38f7c54c42c4e8f7c8985c1a3d321fa9
Raphael Bravim <raphael.bravim.ctr@i-systems.com>
2023-08-09 20:47:32 -0300

    #16-bug-forgot-endpoint

--- app/auth/controllers/forgot.py
+++ app/auth/controllers/forgot.py
@@ -1,6 +1,8 @@
 from flask import request, render_template, session, redirect, url_for, flash
 
 from app.database.models import User
+from app.library.mail import send_email
+from app.library.validation import encode_jwt
 
 
 def forgot_controller():
@@ -13,31 +15,29 @@ def forgot_controller():
         user = User.query.filter_by(use_login=use_login).first()
 
         if user:
-            session.clear()
-            session['user_id'] = user.id
-            return redirect(url_for('templates/home/pages/success.html'))
+            if user.use_is_valid:
 
-        error = 'Usuário não encontrado!'
+                jwt_payload = encode_jwt({
+                    'user_id': user.id,
+                    'use_login': user.use_login
+                })
+                link_senha = f'https://dashflux.com.br/recovery?k={jwt_payload}'
+                send_email(
+                    use_login,
+                    'Instruções para Redefinir Senha',
+                    f"Para redefinir sua senha, favor seguir por este link: {link_senha}"
+                )
+
+                session.clear()
+                session['user_id'] = user.id
+                return render_template('auth/pages/success.html', email_sent=True)
+
+            else:
+                session['error'] = 'Sua verificação está pendente, favor concluir'
+                session['mail'] = use_login
+                redirect(url_for('auth.verify'))
+
+        error = 'Usuário não cadastrado!'
 
         flash(error)
         return render_template('auth/pages/forgot.html', error=error)
-
-    # if request.method == 'POST':
-    #     use_login = request.form['email']
-    #     database = get_db()
-    #     error = None
-    #     user = database.execute(
-    #         'SELECT * FROM board WHERE use_login = ?', use_login
-    #     ).fetchone()
-    #
-    #     if user is None:
-    #         error = 'E-mail is not registered.'
-    #
-    #     if error is None:
-    #         session.clear()
-    #         session['user_id'] = user['id']
-    #         return redirect(url_for('templates/auth/pages/success.html'))
-    #
-    #     flash(error)
-    #
-    # return render_template('auth/pages/forgot.html')
