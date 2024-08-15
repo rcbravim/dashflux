2febb23330a7e4d0ef7e1af2e19c5c82e7774592
Raphael Bravim <raphael.bravim@gmail.com>
2024-08-15 10:49:06 -0300

    ajuste de rota: add prefix para arquivos utilizando static

--- app/board/routes.py
+++ app/board/routes.py
@@ -5,7 +5,8 @@ from flask_login import login_required
 
 from .controllers import *
 
-bp = Blueprint('board', __name__, url_prefix=f'/{os.getenv("ENVIRONMENT", "")}/board')
+env = os.getenv('ENVIRONMENT', '')
+bp = Blueprint('board', __name__, url_prefix=f'/{env}/board', static_folder='static', static_url_path=f'/{env}/static')
 
 
 @bp.before_request
