afddd6afaedd719e84c9d1956a9a21ac28fb6446
Raphael Bravim <raphael.bravim@gmail.com>
2025-02-11 18:19:17 -0300

    adjust lenght column database

--- app/database/models.py
+++ app/database/models.py
@@ -7,7 +7,7 @@ from app.database.database import db
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     use_login = db.Column(db.String(250), nullable=False, unique=True)
-    use_password = db.Column(db.String(128), nullable=False)
+    use_password = db.Column(db.String(250), nullable=False)
     use_status = db.Column(db.Boolean, nullable=False, default=True)
     use_date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     use_date_updated = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
